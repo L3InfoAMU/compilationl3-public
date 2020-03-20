fafecf405dacdc711075029477f73541d865a342
krisy98 <christine.cozzolino@etu.univ-amu.fr>
2020-03-20 12:18:28 +0100

    tp4 (c3a) en cours

--- src/Sa2c3a.java
+++ src/Sa2c3a.java
@@ -1,9 +1,8 @@
 import c3a.*;
-import sa.SaAppel;
-import sa.SaDepthFirstVisitor;
-import sa.SaNode;
-import sa.SaProg;
+import sa.*;
 import ts.Ts;
+import ts.TsItemFct;
+import ts.TsItemVar;
 
 public class Sa2c3a extends SaDepthFirstVisitor <C3aOperand> {
 
@@ -32,6 +31,49 @@ public class Sa2c3a extends SaDepthFirstVisitor <C3aOperand> {
 
         return c3aFunction;
     }
+
+    public C3aOperand visit(SaDecFonc node) {
+        TsItemFct fct = table.getFct(node.getNom());
+        c3a.ajouteInst(new C3aInstFBegin(fct, ""));
+
+        node.getCorps().accept(this);
+
+        c3a.ajouteInst(new C3aInstFEnd(""));
+        return null;
+    }
+
+    //On ne se préoccupe pas des déclarations de variable
+    public C3aOperand visit(SaDecTab node) {
+        return null;
+    }
+
+    public C3aOperand visit(SaDecVar node) {
+        return null;
+    }
+
+    public C3aOperand visit(SaVarIndicee node) {
+        C3aOperand indice = node.getIndice().accept(this);
+        C3aVar var;
+
+        if(!(indice.getClass().equals(C3aConstant.class) || indice.getClass().equals(C3aTemp.class))) {
+            C3aTemp c3aTemp = c3a.newTemp();
+
+            c3a.ajouteInst(new C3aInstAffect(indice, c3aTemp, ""));
+
+            var = new C3aVar((TsItemVar) node.tsItem, c3aTemp);
+        }
+        else {
+            var = new C3aVar((TsItemVar) node.tsItem, indice);
+        }
+        return var;
+    }
+
+    public C3aOperand visit(SaVarSimple node) {
+        return new C3aVar((TsItemVar) node.tsItem, null);
+    }
+
+
+
 
 
 
